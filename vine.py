6396dadd239c05ef62575235c9d85fd7669855fc
daltonypng <antonio.almeidaaa05@gmail.com>
2022-09-22 15:09:50 -0300

    Adicionado busca de arquivos em multithread no vine

--- vine.py
+++ vine.py
@@ -1,11 +1,13 @@
 from os import walk
 import queue
+import threading
+import time
 
 encodingsList = ["utf8", "cp1252", "iso-8859-1", "iso-8859-2"]
 maxExcondingsSuported = len(encodingsList)
 
 search_queue = queue.Queue()
-result_queue = queue.Queue()
+event = threading.Event()
 
 def try_open(file, n):
 
@@ -25,8 +27,11 @@ def try_open(file, n):
  
 def search_worker():
 
-    while not search_queue.empty():
-        
+    while True: 
+
+        if event.is_set() and search_queue.empty():
+            return 
+
         item = search_queue.get_nowait()
         fileContent = try_open(item, 0)
         n = 0
@@ -34,8 +39,8 @@ def search_worker():
         for line in fileContent:
             n += 1
             if search_pattern in line: 
-                result_queue.put(item + ":" + str(n) + " " + line)
-        
+                print(item + ":" + str(n) + " " + line)
+
 def seek_and_destroy(path, extension):
 
     search_len = len(extension) * -1
@@ -48,7 +53,9 @@ def seek_and_destroy(path, extension):
         for fileName in filenames:
             if fileName[search_len:] == extension:
                 search_queue.put(dirpath + fileName)
-    
+
+    event.set()
+
 def vine():
 
     path = input("File path: ")
@@ -61,12 +68,10 @@ def vine():
         extension = "." + extension
 
     seek_and_destroy(path, extension)
-    search_worker()
-
+    
     print("---------------------")
- 
-    while not result_queue.empty():
-        print(result_queue.get_nowait())
+
+    threading.Thread(target=search_worker).start()
 
 if __name__ == "__main__":
 
